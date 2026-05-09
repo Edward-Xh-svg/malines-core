50b3675fc9adef2061011ac47fb755d4bf023ef6
Misha-Chan <alghafourimaryam@gmail.com>
2026-05-09 12:47:08 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,16 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-// خدمة الملفات الثابتة من مجلد public و المجلدات الأخرى
+// خدمة ملفات الموقع الأساسية
 app.use(express.static(path.join(__dirname, 'public')));
+
+// خدمة المجلدات الخارجية للوصول إليها عبر المتصفح
 app.use('/TXT', express.static(path.join(__dirname, 'TXT')));
 app.use('/PNG', express.static(path.join(__dirname, 'PNG')));
 
-// المسار الرئيسي
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// مسار الويكي
-app.get('/wiki', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'wiki.html'));
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+app.listen(3000, () => console.log('Server running on port 3000'));
